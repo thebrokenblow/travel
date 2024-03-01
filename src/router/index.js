ebc7551daee7269808c66d505b8999f074230140
Artem Krasov <artkrasov@gmail.com>
2024-03-01 22:07:51 +0300

    Delet "about" page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
-import About from '@/views/About.vue'
 import Brazil from '@/views/Brazil.vue'
 import Hawaii from '@/views/Hawaii.vue'
 import Jamaica from '@/views/Jamaica.vue'
@@ -8,7 +7,6 @@ import Panama from '@/views/Panama.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
-  { path: '/about', name: 'About', component: About },
   { path: '/brazil', name: 'brazil', component: Brazil },
   { path: '/hawaii', name: 'hawaii', component: Hawaii },
   { path: '/jamaica', name: 'jamaica', component: Jamaica },
