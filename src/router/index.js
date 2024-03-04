ce98956e6aa8ba4c2ac0e13301db03997550d711
Artem Krasov <artkrasov@gmail.com>
2024-03-04 20:31:05 +0300

    реализовал навигацию и удалил ненужные файлы

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
-import Brazil from '@/views/Brazil.vue'
-import Hawaii from '@/views/Hawaii.vue'
-import Jamaica from '@/views/Jamaica.vue'
-import Panama from '@/views/Panama.vue'
-import DestinationShow from '@/views/DestinationShow.vue'
 
 const routes = [
-  { path: '/', name: 'home', component: Home },
-  { path: '/brazil', name: 'brazil', component: Brazil },
-  { path: '/hawaii', name: 'hawaii', component: Hawaii },
-  { path: '/jamaica', name: 'jamaica', component: Jamaica },
-  { path: '/panama', name: 'panama', component: Panama },
-  { path: '/description/:id', name: 'destination.show', component: DestinationShow }
+  { path: '/', name: 'Home', component: Home },
+  {
+    path: '/destination/:id/:slug',
+    name: 'destination.show',
+    component: () => import('@/views/DestinationShow.vue')
+  }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes
 })
-
 export default router
