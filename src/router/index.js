28f5c6e60db2fbf13adf653489f2a11910cae9b7
Artem Krasov <artkrasov@gmail.com>
2024-03-04 20:10:14 +0300

    Добавил детали страны

--- src/router/index.js
+++ src/router/index.js
@@ -4,13 +4,15 @@ import Brazil from '@/views/Brazil.vue'
 import Hawaii from '@/views/Hawaii.vue'
 import Jamaica from '@/views/Jamaica.vue'
 import Panama from '@/views/Panama.vue'
+import DestinationShow from '@/views/DestinationShow.vue'
 
 const routes = [
-  { path: '/', name: 'Home', component: Home },
+  { path: '/', name: 'home', component: Home },
   { path: '/brazil', name: 'brazil', component: Brazil },
   { path: '/hawaii', name: 'hawaii', component: Hawaii },
   { path: '/jamaica', name: 'jamaica', component: Jamaica },
-  { path: '/panama', name: 'panama', component: Panama }
+  { path: '/panama', name: 'panama', component: Panama },
+  { path: '/description/:id', name: 'destinationShow', component: DestinationShow }
 ]
 
 const router = createRouter({
