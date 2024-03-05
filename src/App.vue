7bb6d8a0516a07000ea47e6c223642db40e662fc
Artem Krasov <artkrasov@gmail.com>
2024-03-05 14:09:58 +0300

    Добавил динамическое получение данных

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <the-navigation />
   <div class="container">
-    <router-view></router-view>
+    <router-view :key="$route.path"></router-view>
   </div>
 </template>
 
--- src/views/DestinationShow.vue
+++ src/views/DestinationShow.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="destination">
+  <section v-if="destination" class="destination">
     <h1>{{ destination.name }}</h1>
     <div class="destination-details">
       <img :src="`/images/${destination.image}`" :alt="destination.name" />
@@ -10,13 +10,21 @@
 <script>
 import sourceData from '@/data.json'
 export default {
-  computed: {
-    destinationId() {
-      return parseInt(this.$route.params.id)
-    },
-    destination() {
-      return sourceData.destinations.find((destination) => destination.id === this.destinationId)
+  data() {
+    return {
+      destination: null
     }
+  },
+  methods: {
+    async initData() {
+      const response = await fetch(
+        `https://travel-dummy-api.netlify.app/${this.$route.params.slug}.json`
+      )
+      this.destination = await response.json()
+    }
+  },
+  async created() {
+    this.initData()
   }
 }
 </script>
