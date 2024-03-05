c4846ec59d221168b8f6775253c84c1b7f8289b1
Artem Krasov <artkrasov@gmail.com>
2024-03-05 19:46:19 +0300

    Добавил отображение ошибки при неправильном пути

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory, useRoute } from 'vue-router'
 import Home from '@/views/Home.vue'
+import sourceData from '@/data.json'
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
@@ -7,13 +8,33 @@ const routes = [
     path: '/destination/:id/:slug',
     name: 'destination.show',
     component: () => import('@/views/DestinationShow.vue'),
-    props: (route) => ({ ...route.params, id: parseInt(route.params.id) })
+    props: (route) => ({ ...route.params, id: parseInt(route.params.id) }),
+    beforeEnter(to, from) {
+      const exists = sourceData.destinations.find(
+        (destination) => destination.id === parseInt(to.params.id)
+      )
+      if (!exists)
+        return {
+          name: 'NotFound',
+          // allows keeping the URL while rendering a different page
+          params: { pathMatch: to.path.split('/').slice(1) },
+          query: to.query,
+          hash: to.hash
+        }
+    },
+    children: [
+      {
+        path: ':experienceSlug',
+        name: 'experience.show',
+        component: () => import('@/views/ExperienceShow.vue'),
+        props: (route) => ({ ...route.params, id: parseInt(route.params.id) })
+      }
+    ]
   },
   {
-    path: '/destination/:id/:slug/:experienceSlug',
-    name: 'experience.show',
-    component: () => import('@/views/ExperienceShow.vue'),
-    props: (route) => ({ ...route.params, id: parseInt(route.params.id) })
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: () => import('@/views/NotFound.vue')
   }
 ]
 
